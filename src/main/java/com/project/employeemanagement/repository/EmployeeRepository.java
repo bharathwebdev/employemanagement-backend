97ab9d9f00aca71252cc7e789c6e63c3edb785bf
bharath <bharath18117886@gmail.com>
2023-11-19 23:47:55 +0530

    updated search and filter

--- src/main/java/com/project/employeemanagement/repository/EmployeeRepository.java
+++ src/main/java/com/project/employeemanagement/repository/EmployeeRepository.java
@@ -1,6 +1,8 @@
 package com.project.employeemanagement.repository;
 
 import com.project.employeemanagement.model.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-        List<Employee> findByFullName(String name);
-        }
+        List<Employee> findByFullName(String name, Pageable pageable);
+
+        Page<Employee> findBySalaryGreaterThanEqualAndAgeGreaterThanEqual(double minSalary, int minAge, Pageable pageable);
+}
